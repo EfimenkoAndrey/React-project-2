70e273374186bdafe33b974c5cba8329cb00e50f
EfimenkoAndrey <andrey.efimenko.sp@gmail.com>
2023-06-07 14:45:58 +0300

    feat: add section info

--- react-project-2/src/App.js
+++ react-project-2/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Info from './components/Info/Info';
 
 function App() {
   return (
     <div className="App">
+      <Info />
     </div>
   );
 }
--- react-project-2/src/components/Info/Info.js
+++ react-project-2/src/components/Info/Info.js
@@ -1,1 +1,52 @@
-import 'info.css';
+import './info.css';
+import React from 'react';
+import MyPhoto from '../../images/my_photo.jpg';
+import email from '../../images/svg/email.svg';
+import linkedin from '../../images/svg/linkedin.svg';
+
+export default function Info() {
+  return (
+    <section className='Info'>
+      <img
+        src = { MyPhoto }
+        alt = 'My_logo'
+        className='Info--logo'
+      />
+      <div className='info--wrapper'>
+        <h1 className='Info-title'>Andrei Yafimenka</h1>
+        <h4 className='info-occupation'>Frontend Developer</h4>
+        <a 
+        target='blank'
+        href='https://github.com/EfimenkoAndrey'
+        className='info--link'
+        >
+        https://github.com/EfimenkoAndrey
+        </a>
+        <div className='button-box'>
+          <a
+          href='https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox'
+          className='info--btn'
+          >
+            <img
+            src= { email }
+            alt='Email'
+            className='btn-img'
+            />
+            Email
+          </a>
+          <a
+          href='https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox'
+          className='info--btn info--btn_bg'
+          >
+            <img
+            src= { linkedin }
+            alt='Email'
+            className='btn-img'
+            />
+            Linkedin
+          </a>
+        </div>
+      </div>
+    </section>
+  )
+}
