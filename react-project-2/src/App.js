311c2f719ae0ab8f68ba73ebb5220deeb772900a
EfimenkoAndrey <andrey.efimenko.sp@gmail.com>
2023-06-07 22:47:41 +0300

    feat: add section about

--- react-project-2/src/App.js
+++ react-project-2/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import About from './components/About/About';
 import Info from './components/Info/Info';
 
 function App() {
   return (
     <div className="App">
       <Info />
+      <About />
     </div>
   );
 }
--- react-project-2/src/components/About/About.js
+++ react-project-2/src/components/About/About.js
@@ -1,1 +1,11 @@
-import 'about.css';
+import './about.css';
+import React from 'react';
+
+export default function About() {
+  return (
+    <section className='About'>
+      <h2 className='section-title'>About</h2>
+      <p className='section-content'>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+    </section>
+  )
+}
