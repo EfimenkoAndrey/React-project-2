313f46308e25eab3aee3b3f06225baeff73ea7a6
EfimenkoAndrey <andrey.efimenko.sp@gmail.com>
2023-06-07 22:55:37 +0300

    feat: add section interests

--- react-project-2/src/App.js
+++ react-project-2/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import About from './components/About/About';
 import Info from './components/Info/Info';
+import Interests from './components/Interests/Interests';
 
 function App() {
   return (
     <div className="App">
       <Info />
       <About />
+      <Interests />
     </div>
   );
 }
--- react-project-2/src/components/Interests/Interests.js
+++ react-project-2/src/components/Interests/Interests.js
@@ -1,1 +1,11 @@
-import 'interests.css';
+import './interests.css';
+import React from 'react';
+
+export default function Interests() {
+  return (
+    <section className='Interests'>
+      <h2 className='section-title'>Interests</h2>
+      <p className='section-content'>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </section>
+  )
+}
